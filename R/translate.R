b291abced69df6f1414f16603f5c8f02a808ba9e
Dannie-JO <danniejo@live.dk>
2025-10-28 11:42:59 +0100

    translate

--- R/translate.R
+++ R/translate.R
@@ -1,8 +1,8 @@
 #' select_ribo
-#' Select ribosome codons and concatenate them
+#' Select ribosome codons and makes them into AA
 #'
 #' This function selects specific codons from the `ribo` vector and
-#' concatenates them into a single string with no separator.
+#' makes them into Aminoacids
 #'
 #' @param codons A numeric or character vector indicating which codons to select from `ribo`.
 #' @returns A character string containing the selected ribosome codons concatenated together.
@@ -12,7 +12,7 @@
 #' ribo <- c("AUG", "GCU", "UAA", "CGG")
 #' codons <- c(1, 2, 4)
 #' select_ribo(codons)
-#' # Returns: "AUGGCUCGG"
+#' # Returns: ""
 select_ribo <- function(codons){
   select_ribo2 <- paste0(ribo[codons], collapse = "")
   return(select_ribo2)
