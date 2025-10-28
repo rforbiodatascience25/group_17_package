a13c0c5eb4f11cc4665b292966e5eacdda9d5dfb
s235350 <s235350@dtu.dk>
2025-10-28 10:06:38 +0100

    Ribo added

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+#'
+#' Description
+#' Contains the standard translation table from RNA codon to amino acid
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"ribo"
